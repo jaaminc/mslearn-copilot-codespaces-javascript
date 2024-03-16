1e84226ad5dbd581d1c5891962a8e09cb2f15c4e
jaaminc <163538837+jaaminc@users.noreply.github.com>
2024-03-16 21:40:11 +0000

    Custom 1

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
+  name: "Jose.A",
+  title: "Software Engineer",
+  email: "josea@example.com",
   gitHub: "microsoft",
   instagram: "microsoft",
-  linkedIn: "satyanadella",
+  linkedIn: "mind",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
